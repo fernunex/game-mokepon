c4f31cb7a047243505776facf0ef8929c1e941dd
fernunex <fer.bsof@outlook.com>
2023-05-31 13:32:54 -0600

    Class 75 Installing Express and creating server web

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hola Node')
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res) => { 
+    res.send("Hola")
+})
+
+
+// listen at port 8070
+app.listen(8070, () => {
+    console.log('El server esta funcionando')
+})
